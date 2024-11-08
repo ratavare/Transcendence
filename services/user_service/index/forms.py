0554cfb23723e6f8a7b267ff1b7066a267584b54
ddiniz-m <diogodinizm@gmail.com>
2024-11-08 17:13:46 +0000

    Fixed user form password not hidding input. Fixed profile model error when making magritions or when creating super user

--- services/user_service/index/forms.py
+++ services/user_service/index/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
 	username = forms.CharField(required=True, label="Username")
-	password1 = forms.CharField(required=True, label="Password")
-	password2 = forms.CharField(required=True, label="Password Confirmation")
+	password1 = forms.CharField(required=True, label="Password", widget=forms.PasswordInput)
+	password2 = forms.CharField(required=True, label="Password Confirmation", widget=forms.PasswordInput)
 
 	class Meta:
 		model = User
