c3056d0c181d9d587ca9435cf96f281eb4eacb14
ddiniz-m <diogodinizm@gmail.com>
2024-11-14 19:41:42 +0000

    Added html when searching for a friend

--- services/frontend/profile/static/js/friends.js
+++ services/frontend/profile/static/js/friends.js
@@ -1,9 +1,27 @@
 
 function putUsers(users)
 {
+	const previousList = userListDiv.querySelector('ul');
+	previousList?.remove();
+	const userList = document.createElement('ul');
+	userList.classList.add("list-group");
 	users.forEach(user => {
-		console.log("User:", user);
+		const userItemList = document.createElement('li');
+		userItemList.textContent = user.username;
+		userItemList.classList.add("list-group-item");
+		userItemList.style = 'display: flex;align-items: center;justify-content: space-around';
+	
+		const friendRequestButton = document.createElement('button');
+		friendRequestButton.classList.add("btn", "btn-secondary", "col", "pull-right");
+		friendRequestButton.textContent = "Send Friend Request";
+		friendRequestButton.style.display = 'flex';
+
+		userItemList.appendChild(friendRequestButton);
+		userList.appendChild(userItemList);
+
 	});
+	userListDiv.appendChild(userList);
+	userListDiv.style.display = 'block';
 }
 
 const userListDiv = document.getElementById('user-search-result');
@@ -15,15 +33,15 @@ userListDiv.style.display = 'none';
 	formUsers?.addEventListener('submit', function(event) {
 		event.preventDefault();
 
-		console.log("TARGET:", event.target);
 		const formData = new FormData(event.target);
 
 		const fetch_url = 'https://localhost:8443/user_auth/user_search/';
 		myFetch(fetch_url, formData)
 		.then(data => {
 			putUsers(data.users);
+			
 		}).catch(error => {
-			console.log('Error:', error);
+			console.log(error);
 		})
 	});
 }
