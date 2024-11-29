fc78ae8e59fa347bc16dca109fc992fbeef9df3a
ddiniz-m <diogodinizm@gmail.com>
2024-11-29 12:29:10 +0000

    Program now creates migrations and applies them on start

--- services/backend/game/script.sh
+++ services/backend/game/script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+sleep 4
+
+BOLD_ORANGE="\e[1;33m"
+NC="\e[0m"
+
+#Exits if any command exits with an error (!= 0)
+set -e
+
+echo "${BOLD_ORANGE}Creating migrations...${NC}"
+python manage.py makemigrations
+
+echo "${BOLD_ORANGE}Migrating...${NC}"
+python manage.py migrate
+
+echo "${BOLD_ORANGE}Starting the server...${NC}"
 python manage.py runserver 0.0.0.0:8002
 daphne -p 8002 game.asgi:application
---
+++ services/backend/user_management/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sleep 2
+
+BOLD_BLUE="\e[1;34m"
+NC="\e[0m"
+
+#Exits if any command exits with an error (!= 0)
+set -e
+
+echo "${BOLD_BLUE}Creating migrations...${NC}"
+python manage.py makemigrations
+
+echo "${BOLD_BLUE}Migrating...${NC}"
+python manage.py migrate
+
+echo "${BOLD_BLUE}Starting the server...${NC}"
+python manage.py runserver 0.0.0.0:8001
