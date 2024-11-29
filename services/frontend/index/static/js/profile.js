6565d7ade091292d666e08966ad5bb214dc41993
Tereza Melo Ribeiro <terezameloribeiro@gmail.com>
2024-11-29 17:22:54 +0000

    29/11 v0

--- services/frontend/index/static/js/profile.js
+++ services/frontend/index/static/js/profile.js
@@ -7,9 +7,48 @@ async function fillProfile(){
 		inputs[1].value = window.user.email;
 		inputs[2].value = window.user.bio;
 		inputs[3].value = window.user.birth_date;
+		document.getElementById('username').innerText = window.user.username;
+        document.getElementById('fullname').innerText = window.user.full_name || "";
+        document.getElementById('email').innerText = window.user.email;
+        document.getElementById('bio').innerText = window.user.bio || "";
+        document.getElementById('city').innerText = window.user.city || "";
+        document.getElementById('birth-date').innerText = window.user.birth_date;
+
+        document.getElementById('username-edit').value = window.user.username;
+        document.getElementById('id-email').value = window.user.email;
+        document.querySelector('input[name="bio"]').value = window.user.bio || "";
+        document.querySelector('input[name="city"]').value = window.user.city || "";
+        document.getElementById('id-date').value = window.user.birth_date;
 	} catch (error) {
 		console.error("Fetch error: ", error);
 	}
+}
+
+function editProfile() {
+    document.getElementById('profile-view').style.display = 'none';
+    document.getElementById('profile-edit').style.display = 'block';
+
+    document.getElementById('username-edit').value = window.user.username;
+	document.getElementById('id-email').value = window.user.email;
+	document.querySelector('input[name="bio"]').value = window.user.bio || "";
+	document.querySelector('input[name="city"]').value = window.user.city || "";
+	document.getElementById('id-date').value = window.user.birth_date;
+}
+
+// Function to save changes and return to view mode
+async function saveProfile() {
+	const form = document.getElementById('form-profile');
+	const formData = new FormData(form);
+
+		try {
+			await myFetch('https://localhost:8443/user_profile/profile/', formData)
+			console.log("Profile change successful");
+			seturl('/home');
+		} catch(error) {
+			console.log("Profile change failed. Reason:", error);
+			alert("Profile change failed. Reason:", error);
+		}
+
 }
 
 {
