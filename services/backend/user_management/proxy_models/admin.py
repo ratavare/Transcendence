681a1ff5bced802f5449f6c847c5cb6cbf7be869
ratavare <rafaelftcardoso@gmail.com>
2024-12-03 22:00:17 +0000

    Added decorator to admin.py instead of registering to admin site manually

--- services/backend/user_management/proxy_models/admin.py
+++ services/backend/user_management/proxy_models/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import ProxyLobby
 
+@admin.register(ProxyLobby)
 class ProxyLobbyAdmin(admin.ModelAdmin):
 	list_display = ('lobby_id', 'created_at', 'user_list')
 
 	def user_list(self, obj):
-		users = obj.get_users()  # Ensure this method exists in the ProxyLobby model
+		users = obj.get_users()
 		return ", ".join(users) if users else "No users"
 	user_list.short_description = "Users"
-
-admin.site.register(ProxyLobby, ProxyLobbyAdmin)
-
