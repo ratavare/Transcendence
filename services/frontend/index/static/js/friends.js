a15d9d3317b34e094c9b563d28de39bd96411672
Tereza Melo Ribeiro <terezameloribeiro@gmail.com>
2024-12-04 18:23:33 +0000

    04/12 friends v0

--- services/frontend/index/static/js/friends.js
+++ services/frontend/index/static/js/friends.js
@@ -62,6 +62,29 @@ function putUsers(users)
 	userListDiv.style.display = 'block';
 
 	sendButtonConfigure();
+
+	/* const previousList2 = document.getElementById('friends-list2');
+	previousList2?.remove();
+	const membersContainer = document.createElement('div');
+	membersContainer.classList.add("row"); */
+	const membersContainer = document.getElementById("friends-list2"); // Update to match your HTML structure
+	users.forEach(user => {
+		const card = document.createElement("div");
+		card.className = "col-sm-6 col-lg-4";
+		card.innerHTML = `
+			<div class="card hover-img">
+				<div class="card-body p-4 text-center border-bottom">
+					<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="rounded-circle mb-3" width="80" height="80">
+					<h5 class="fw-semibold mb-0">@${user.username}</h5>
+				</div>
+				<div class="px-2 py-2 bg-light text-center">
+					<button class="btn btn-success me-2" onclick="handleFriendRequest('accept', '${user.username}')">Accept</button>
+					<button class="btn btn-danger" onclick="handleFriendRequest('decline', '${user.username}')">Decline</button>
+				</div>
+			</div>
+		`;
+		membersContainer.appendChild(card);
+	});
 }
 
 const friendsListDiv = document.getElementById('user-friends');
