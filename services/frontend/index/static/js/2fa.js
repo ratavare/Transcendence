2c19233e9472da76f7abe2df8b936d868d3b04ae
Henrique Moraes <kikovillamoraes@gmail.com>
2024-12-05 14:41:16 +0000

    Now a user can use Setup Key to enable 2FA instead of QR Code; Small adjustments.

--- services/frontend/index/static/js/2fa.js
+++ services/frontend/index/static/js/2fa.js
@@ -1,20 +1,21 @@
-console.log("fuck this " + localStorage.getItem('is_2fa_enabled') === 'true');
+console.log("2FA enabled status: " + (localStorage.getItem('is_2fa_enabled') === 'true'));
 if (localStorage.getItem('is_2fa_enabled') === 'true') {
-	document.getElementById('form-2fa-setup').style.display = 'none';
-	document.getElementById('2fa-status').style.display = 'block';
+    document.getElementById('2fa-setup').style.display = 'none';
+    document.getElementById('2fa-status').style.display = 'block';
 } else {
-	document.getElementById('form-2fa-setup').style.display = 'block';
-	document.getElementById('2fa-status').style.display = 'none';
+    document.getElementById('2fa-setup').style.display = 'block';
+    document.getElementById('2fa-status').style.display = 'none';
 }
 
-document.getElementById('form-2fa-setup').addEventListener('submit', async function(event) {
-    event.preventDefault();
+document.getElementById('enable-2fa-button').addEventListener('click', async function() {
     try {
         const data = await myFetch('https://localhost:8443/user_auth/enable_2fa/', null, 'POST', true);
         if (data.qr_code) {
             document.getElementById('qr-container').innerHTML = `<img src="data:image/png;base64,${data.qr_code}" />`;
+            document.getElementById('setup-key').textContent = data.otp_secret;
+            document.getElementById('setup-key-container').style.display = 'block';
             document.getElementById('otp-container').style.display = 'block';
-			localStorage.setItem('otp_secret', data.otp_secret);
+            localStorage.setItem('otp_secret', data.otp_secret);
         } else {
             alert('Failed to activate 2FA.');
         }
@@ -25,17 +26,18 @@ document.getElementById('form-2fa-setup').addEventListener('submit', async funct
 
 document.getElementById('verify-otp-button').addEventListener('click', async function() {
     const otpCode = document.getElementById('otp-code').value;
-	const otpSecret = localStorage.getItem('otp_secret');
+    const otpSecret = localStorage.getItem('otp_secret');
     try {
         const data = await myFetch('https://localhost:8443/user_auth/verify_otp/', { otp: otpCode, otp_secret: otpSecret }, 'POST', true);
         if (data.status === 'success') {
             document.getElementById('qr-container').style.display = 'none';
+            document.getElementById('setup-key-container').style.display = 'none';
             document.getElementById('otp-container').style.display = 'none';
-            document.getElementById('form-2fa-setup').style.display = 'none';
+            document.getElementById('2fa-setup').style.display = 'none';
             document.getElementById('2fa-status').style.display = 'block';
             localStorage.setItem('is_2fa_enabled', 'true');
-			localStorage.removeItem('otp_secret');
-			alert('Successfully enabled 2FA');
+            localStorage.removeItem('otp_secret');
+            alert('Successfully enabled 2FA');
         } else {
             alert('Invalid OTP code.');
         }
@@ -50,7 +52,7 @@ document.getElementById('disable-2fa-button').addEventListener('click', async fu
         if (data.status === 'success') {
             alert('2FA has been disabled.');
             document.getElementById('2fa-status').style.display = 'none';
-            document.getElementById('form-2fa-setup').style.display = 'block';
+            document.getElementById('2fa-setup').style.display = 'block';
             localStorage.setItem('is_2fa_enabled', 'false');
         } else {
             alert('Failed to disable 2FA.');
