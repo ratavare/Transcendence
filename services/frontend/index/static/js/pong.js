94896fb228c4eba7e2c98540289b7e6f5b7e8e41
ddiniz-m <diogodinizm@gmail.com>
2024-12-13 01:01:57 +0000

    Deleted threejs files, now importing using CDN. Added background and ball texture to pong

--- services/frontend/index/static/js/pong.js
+++ services/frontend/index/static/js/pong.js
@@ -4,15 +4,10 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 PageElement.onLoad = () => {
 	// Constants
-	const PADDLE_SPEED = 15;
-	const AIPADDLE_SPEED = 15;
-	const BALL_INITIAL_SPEED = -10;
 	const SHAKE_INTENSITY = 10;
-	const SHAKE_DURATION = 5;
 	const PADDLE_COLOR = 0x008000;
 	const TABLE_COLOR = 0x800080;
 	const PLANE_COLOR = 0x000000;
-	const BALL_COLOR = 0x00ff00;
 	const POINT_LIGHT_INTENSITY = 1000000;
 	const POINT_LIGHT_DISTANCE = 1000;
 	const AMBIENT_LIGHT_INTENSITY = 3;
@@ -21,8 +16,6 @@ PageElement.onLoad = () => {
 	let player1Score = 0;
 	let player2Score = 0;
 	let shakeDuration = 0;
-	// let sphereData = [];
-	// let startTime = Date.now();
 
 	// Scene Setup
 	const canvas = document.getElementById('canvas');
@@ -36,41 +29,48 @@ PageElement.onLoad = () => {
 	camera.position.set(0, 500, 0);
 	controls.update();
 
-	// Helpers
-	// const gridHelper = new THREE.GridHelper(10000, 100, 0x808080);
-	// scene.add(gridHelper);
-
-	// Axes
-	// const axesHelper = new THREE.AxesHelper(1000);
-	// scene.add(axesHelper);
-
-
 	// Plane
-	const planeGeometry = new THREE.PlaneGeometry(3000, 2000);
+	/* const planeGeometry = new THREE.PlaneGeometry(3000, 2000);
 	const planeMaterial = new THREE.MeshStandardMaterial({ color: PLANE_COLOR });
 	const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 	plane.rotation.x = -Math.PI / 2;
 	plane.position.y = -100;
 	plane.receiveShadow = true;
-	scene.add(plane);
+	scene.add(plane); */
+
+	// Skybox
+	const loader = new THREE.CubeTextureLoader();
+	const skybox = loader.load([
+		'media/skybox/right.png', // Right
+		'media/skybox/left.png', // Left
+		'media/skybox/top.png', // Top
+		'media/skybox/bottom.png', // Bottom
+		'media/skybox/front.png', // Front
+		'media/skybox/back.png'  // Back
+	]);
+	scene.background = skybox;
+	
+	// Sphere
+	const ballLoader = new THREE.TextureLoader();
+	const ballTexture = ballLoader.load('media/skybox/grey-scale-sun.jpg');
+	const sphereMaterial = new THREE.MeshBasicMaterial({
+		map: ballTexture,
+		color: 0x33e3ff,
+	});
+	const sphereGeometry = new THREE.SphereGeometry(10, 32, 32);
+	const ball = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(ball);
 
 	// Lights
 	const ambientLight = new THREE.AmbientLight(0xffffff, AMBIENT_LIGHT_INTENSITY);
 	scene.add(ambientLight);
 
-	const pointLight = new THREE.PointLight(0xffffff, POINT_LIGHT_INTENSITY, POINT_LIGHT_DISTANCE);
+	const pointLight = new THREE.PointLight(0x33e3ff, POINT_LIGHT_INTENSITY, POINT_LIGHT_DISTANCE);
+	pointLight.position.set(0, 0, 0);
 	scene.add(pointLight);
-
-	// Cube
-	const sphereGeometry = new THREE.SphereGeometry(10, 32, 32);
-	const sphereMaterial = new THREE.MeshStandardMaterial({ color: BALL_COLOR });
-	const ball = new THREE.Mesh(sphereGeometry, sphereMaterial);
-	scene.add(ball);
-	const ballBoundingBox = new THREE.Box3().setFromObject(ball);
-
+	
 	let paddle1 = {};
 	let paddle2 = {};
-
 
 	// Functions
 	function  createEnvironment(data)
@@ -186,200 +186,23 @@ PageElement.onLoad = () => {
 		paddle2.position.z = payloadData.paddle2PositionZ;
 	}
 
-	// function movePaddles()
-	// {
-	// 	paddle1.position.z += paddle1Speed;
-	// 	paddle2.position.z += paddle2Speed;
-	// 	paddle1BoundingBox.setFromObject(paddle1);
-	// 	paddle2BoundingBox.setFromObject(paddle2);
-	// }
-
-	// 	console.log('updatePaddlePositions');
-	// 	if (paddleData.payload.paddle == 1)
-	// 	{
-	// 		paddle1Speed = paddleData.payload.speed;
-	// 	}
-	// 	else if (paddleData.payload.paddle == 2)
-	// 	{
-	// 		paddle2Speed = paddleData.payload.speed;
-	// 	}
-	// 	paddle1BoundingBox.setFromObject(paddle1);
-	// 	paddle2BoundingBox.setFromObject(paddle2);
-
-	// function movePaddles()
-	// {
-	//   paddle1.position.z += paddle1Speed;
-	//   paddle2.position.z += paddle2Speed;
-	//   paddle1BoundingBox.setFromObject(paddle1);
-	//   paddle2BoundingBox.setFromObject(paddle2);
-	// }
-
 	function applyCameraShake() 
 	{
-	if (shakeDuration > 0)
-	{
-		const shakeX = (Math.random() - 0.5) * SHAKE_INTENSITY;
-		const shakeY = (Math.random() - 0.5) * SHAKE_INTENSITY;
-		const shakeZ = (Math.random() - 0.5) * SHAKE_INTENSITY;
-		camera.position.x += shakeX;
-		camera.position.y += shakeY;
-		camera.position.z += shakeZ;
-		shakeDuration--;
+		if (shakeDuration > 0)
+		{
+			const shakeX = (Math.random() - 0.5) * SHAKE_INTENSITY;
+			const shakeY = (Math.random() - 0.5) * SHAKE_INTENSITY;
+			const shakeZ = (Math.random() - 0.5) * SHAKE_INTENSITY;
+			camera.position.x += shakeX;
+			camera.position.y += shakeY;
+			camera.position.z += shakeZ;
+			shakeDuration--;
+		}
+		if (shakeDuration == 0)   
+		{
+			camera.position.set(0, 500, 0); 
+		}
 	}
-	if (shakeDuration == 0)   
-	{
-		camera.position.set(0, 500, 0);
-	}
-	}
-
-	// function increaseSpeed()
-	// {
-	//   if (ballSpeedx < 20 && ballSpeedx > -20)
-	// 	ballSpeedx += (ballSpeedx > 0) ? 0.4 : -0.4;
-	// }
-
-	// function checkIntersections()
-	// {
-	//   ballBoundingBox.setFromObject(ball);
-
-	//   if (ballBoundingBox.intersectsBox(table1BoundingBox) || ballBoundingBox.intersectsBox(table2BoundingBox)) 
-	//   {
-	// 	ballSpeedz *= -1;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (ballBoundingBox.intersectsBox(paddle1BoundingBox))
-	//   {
-	// 	console.log(ballBoundingBox, paddle1BoundingBox)
-	// 	ballSpeedx *= -1;
-	// 	shakeDuration = SHAKE_DURATION;
-	// 	increaseSpeed();
-	// 	adjustCubeDirection(paddle1);
-	// 	ball.position.x += ballSpeedx;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (ballBoundingBox.intersectsBox(paddle2BoundingBox))
-	//   {
-	// 	ballSpeedx *= -1;
-	// 	shakeDuration = SHAKE_DURATION;
-	// 	increaseSpeed();
-	// 	adjustCubeDirection(paddle2);
-	// 	ball.position.x += ballSpeedx;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (paddle1BoundingBox.intersectsBox(table1BoundingBox) || paddle1BoundingBox.intersectsBox(table2BoundingBox)) 
-	//   {
-	// 	paddle1.position.z -= paddle1Speed;
-	//   }
-	//   if (paddle2BoundingBox.intersectsBox(table1BoundingBox) || paddle2BoundingBox.intersectsBox(table2BoundingBox))
-	//   {
-	// 	paddle2.position.z -= paddle2Speed;
-	//   }
-	// }
-
-	// function increaseSpeed()
-	// {
-	//   if (ballSpeedx < 20 && ballSpeedx > -20)
-	// 	ballSpeedx += (ballSpeedx > 0) ? 0.4 : -0.4;
-	// }
-
-	// function checkIntersections()
-	// {
-	//   ballBoundingBox.setFromObject(ball);
-
-	//   if (ballBoundingBox.intersectsBox(table1BoundingBox) || ballBoundingBox.intersectsBox(table2BoundingBox)) 
-	//   {
-	// 	ballSpeedz *= -1;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (ballBoundingBox.intersectsBox(paddle1BoundingBox))
-	//   {
-	// 	console.log(ballBoundingBox, paddle1BoundingBox)
-	// 	ballSpeedx *= -1;
-	// 	shakeDuration = SHAKE_DURATION;
-	// 	increaseSpeed();
-	// 	adjustCubeDirection(paddle1);
-	// 	ball.position.x += ballSpeedx;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (ballBoundingBox.intersectsBox(paddle2BoundingBox))
-	//   {
-	// 	ballSpeedx *= -1;
-	// 	shakeDuration = SHAKE_DURATION;
-	// 	increaseSpeed();
-	// 	adjustCubeDirection(paddle2);
-	// 	ball.position.x += ballSpeedx;
-	// 	ball.position.z += ballSpeedz;
-	//   }
-
-	//   if (paddle1BoundingBox.intersectsBox(table1BoundingBox) || paddle1BoundingBox.intersectsBox(table2BoundingBox)) 
-	//   {
-	// 	paddle1.position.z -= paddle1Speed;
-	//   }
-	//   if (paddle2BoundingBox.intersectsBox(table1BoundingBox) || paddle2BoundingBox.intersectsBox(table2BoundingBox))
-	//   {
-	// 	paddle2.position.z -= paddle2Speed;
-	//   }
-	// }
-
-	// function adjustCubeDirection(paddle)
-	// {
-	//   const relativeIntersectZ = (paddle.position.z + (paddle.geometry.parameters.depth / 2)) - ball.position.z;
-	//   const normalizedIntersectZ = (relativeIntersectZ / (paddle.geometry.parameters.depth / 2)) - 1;
-	//   ballSpeedz = normalizedIntersectZ * 20; // Adjust the multiplier as needed
-	// }
-
-	// function respawnCube(player)
-	// {
-	//   if (player == 1)
-	//   {
-	// 	ball.position.set(200, 0, 0);
-	// 	ballSpeedx = BALL_INITIAL_SPEED;
-	// 	ballSpeedz = 0;
-	// 	beginGame = false;
-	// 	pointLight.position.copy(ball.position);
-	//   }
-	//   else if (player == 2)
-	//   {
-	// 	ball.position.set(-200, 0, 0);
-	// 	ballSpeedx = -BALL_INITIAL_SPEED;
-	// 	ballSpeedz = 0;
-	// 	beginGame = false;
-	// 	pointLight.position.copy(ball.position);
-	//   }
-	// }
-
-	// function ballOutofBounds()
-	// {
-	//   if (ball.position.x > 1000)
-	//   {
-	// 	// console.log('z is ' + ball.position.z + ' x is ' + ball.position.x);
-	// 	respawnCube(1);
-	// 	player1Score++;
-	// 	document.getElementById('player1score').innerHTML = player1Score;
-	//   } else if (ball.position.x < -1000)
-	//   {
-	// 	// console.log('z is ' + ball.position.z + ' x is ' + ball.position.x); 
-	// 	respawnCube(2);
-	// 	player2Score++;
-	// 	document.getElementById('player2score').innerHTML = player2Score;
-	//   }
-	// }
-
-	// function moveCube()
-	// {
-	//   ball.position.x += ballSpeedx;
-	//   ball.position.z += ballSpeedz;
-	//   pointLight.position.copy(ball.position);
-
-	// //   ballOutofBounds();
-
-	// //   ballBoundingBox.setFromObject(ball);	
-	// }
 
 	function updateBall(ballData)
 	{
@@ -388,107 +211,6 @@ PageElement.onLoad = () => {
 		applyCameraShake();
 		pointLight.position.copy(ball.position);
 	}
-
-	// function saveSphereData() 
-	// {
-	//   let time = 0;
-	//   let position = { x: ball.position.x, z: ball.position.z };
-	//   let speed = { x: ballSpeedx, z: ballSpeedz };
-	//   time = Date.now() - startTime;
-
-	//   if (sphereData.length == 0) 
-	//   {
-	// 	sphereData.push({ time: time, position: position, speed: speed });
-	//   }
-	//   else
-	//   {
-	// 	sphereData[0] = { time: time, position: position, speed: speed };
-	//   }
-	// //   console.log('Time: ' + time + ' Position: ' + position.x + ', ' + position.z + ' Speed: ' + speed.x + ', ' + speed.z);
-	// }
-
-	// function calculateTrajectory()  
-	// {
-	//   let data = sphereData[0];
-
-	//   let finalAngle = Math.atan2(data.speed['z'], data.speed['x']);
-	//   finalAngle = finalAngle * (180 / Math.PI);
-
-	//   let distance = 0;
-	//   let finalPosition = { x: 0, z: 0 };
-
-	// 	if (data.speed['x'] > 0) 
-	// 	{
-	// 		distance = 800 - data.position['x'];
-	// 		finalPosition['x'] = 800;
-	// 	} 
-	// 	else if (data.speed['x'] < 0) 
-	// 	{
-	// 		distance = -800 - data.position['x'];
-	// 		finalPosition['x'] = -800;
-	// 	}
-
-	//   // Calculate the distance along the z axis using trigonometric functions
-	//   let angleRadians = Math.atan2(data.speed['z'], data.speed['x']);
-	//   let zDistance = distance * Math.tan(angleRadians);
-	//   finalPosition['z'] = data.position['z'] + zDistance;
-
-	//   // Handle wall bounces
-	//   const topWallZ = -500;
-	//   const bottomWallZ = 500;
-
-	// 	while (finalPosition['z'] < topWallZ || finalPosition['z'] > bottomWallZ) 
-	// 	{
-	// 		if (finalPosition['z'] < topWallZ) 
-	// 		{
-	// 			finalPosition['z'] = topWallZ + (topWallZ - finalPosition['z']);
-	// 		} 
-	// 		else if (finalPosition['z'] > bottomWallZ) 
-	// 		{
-	// 			finalPosition['z'] = bottomWallZ - (finalPosition['z'] - bottomWallZ);
-	// 		}
-	// 	}
-
-	//   return finalPosition;
-	// }
-
-	// function paddle1AI(paddle) 
-	// {
-	//   const topWallZ = -440;
-	//   const bottomWallZ = 440;
-
-	//   // Calculate the final position of the ball using the calculateTrajectory function
-	//   let finalPosition = calculateTrajectory();
-
-	//   // Ensure the paddle moves towards the final position of the ball
-	//   if (ballSpeedx < 0) 
-	//   {
-	// 	if (paddle.position.z > finalPosition['z'])
-	//   {
-	// 	  // Move paddle up
-	// 	  if (paddle.position.z - AIPADDLE_SPEED < topWallZ) 
-	// 	  {
-	// 		paddle.position.z = topWallZ;
-	// 	  }
-	// 	  else if (paddle.position.z - AIPADDLE_SPEED >= finalPosition['z'])
-	// 	  {
-	// 		paddle.position.z -= AIPADDLE_SPEED;
-	// 	  }
-	// 	} 
-	// 	else if (paddle.position.z < finalPosition['z']) 
-	// 	{
-	// 	  // Move paddle down
-	// 	  if (paddle.position.z + AIPADDLE_SPEED > bottomWallZ)
-	// 	  {
-	// 		paddle.position.z = bottomWallZ;
-	// 	  } 
-	// 	  else if (paddle.position.z + AIPADDLE_SPEED <= finalPosition['z']) 
-	// 	  {
-	// 		paddle.position.z += AIPADDLE_SPEED;
-	// 	  }
-	// 	}
-	//   }
-	// }
 
 	// Modify the animate function to include swatting animation logic
 	function animate() 
