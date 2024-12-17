126211803f8ce9f81be4bee6a49e7855e8e46b54
ratavare <rafaelftcardoso@gmail.com>
2024-12-17 19:17:24 +0000

    Changes to frontend, sorry will be my last commit to this branch

--- services/frontend/index/urls.py
+++ services/frontend/index/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
 	path('', views.indexView),
 	path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
