76e9f43c96766e29599b9b59c7f15f659f02b0c4
Tereza Melo Ribeiro <terezameloribeiro@gmail.com>
2025-02-25 00:05:11 +0000

    Revert "add html online status"
    This reverts commit e81f7c99e27da456937dd331b9a0834dce816cb2.

--- services/frontend/index/static/js/onlineState.js
+++ services/frontend/index/static/js/onlineState.js
@@ -1,27 +1,21 @@
 window.getProfile().then(() => {
     const socket = new WebSocket(`wss://localhost:8443/online-status/${window.user.username}/`);
-    socket.onopen = () => {
-        console.log("WebSocket connection established.");
-    };
 
     socket.onmessage = function(event) {
-        console.log("check 0\n");
         const data = JSON.parse(event.data);
         console.log(data);
         updateOnlineStatus(data.online_users);
-    };
+    };    
 })
 
-function updateOnlineStatus(onlineUsers) {
-    const indicators = document.querySelectorAll('.status-indicator');
-    indicators.forEach(indicator => {
-      const username = indicator.getAttribute('data-username');
-      if (onlineUsers.includes(username)) {
-        indicator.classList.remove('status-offline');
-        indicator.classList.add('status-online');
-      } else {
-        indicator.classList.remove('status-online');
-        indicator.classList.add('status-offline');
-      }
-    });
-}
+// function updateOnlineStatus(onlineUsers) {
+//     document.querySelectorAll("[data-username]").forEach(card => {
+//         const username = card.getAttribute("data-username");
+//         const nameElement = card.querySelector("h5");
+//         if (onlineUsers.includes(username)) {
+//             nameElement.innerHTML = `<span style="color: green;">●</span> @${username}`;
+//         } else {
+//             nameElement.innerHTML = `<span style="color: gray;">●</span> @${username}`;
+//         }
+//     });
+// }
